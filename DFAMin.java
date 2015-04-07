76dcb121198129d35ab52001b840e4ac9ccdfb7a
Mark Ignacio <mark@ignacio.io>
2015-04-07 01:49:09 -0400

    sudkamp algorithm finished

--- DFAMin.java
+++ DFAMin.java
@@ -1,5 +1,7 @@
+import java.awt.*;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class DFAMin {
@@ -59,13 +61,20 @@ public class DFAMin {
     private class DFA {
 
         DFAState[] states;
-        int[] acceptStates;
+        HashSet<Integer> acceptStates;
         char[] alphabet;
+
+        private boolean[][] D;
+        private ArrayList<ArrayList<HashSet<Point>>> S;
 
         public DFA(int numAlphabet, int[] acceptStates, DFAState[] states) {
             alphabet = new char[numAlphabet];
             this.states = states;
-            this.acceptStates = acceptStates;
+
+            this.acceptStates = new HashSet<Integer>();
+            for (int acceptState : acceptStates) {
+                this.acceptStates.add(acceptState);
+            }
 
             // fill alphabet
             for (int i = 0; i < numAlphabet; i++) {
@@ -84,6 +93,81 @@ public class DFAMin {
         }
 
         public void minimize() {
+            // Sudkamp minimization algorithm
+
+            // 1. init structures
+            D = new boolean[states.length][states.length];
+            S = new ArrayList<ArrayList<HashSet<Point>>>();  // lol
+
+            for (int i = 0; i < states.length; i++) {
+                ArrayList<HashSet<Point>> innerList = new ArrayList<HashSet<Point>>();
+
+                for (int j = 0; j < states.length; j++) {
+                    innerList.add(new HashSet<Point>());
+                }
+                S.add(innerList);
+            }
+
+            // 2. states with different acceptances are distinguishable
+            for (int i = 0; i < states.length; i++) {
+                for (int j = i + 1; j < states.length; j++) {
+                    if (acceptStates.contains(i) != acceptStates.contains(j)) {
+                        D[i][j] = true;
+                    }
+                }
+            }
+
+            // 3. mark as possibly indistinguishable, enforce distinguishability
+            for (int i = 0; i < states.length; i++) {
+                for (int j = i + 1; j < states.length; j++) {
+                    DFAState qi = states[i];
+                    DFAState qj = states[j];
+
+                    // helps emulate "for any"
+                    boolean distinguished = false;
+                    for (int k = 0; k < qi.transitions.length; k++) {
+                        int qm = qi.transitions[k];
+                        int qn = qj.transitions[k];
+
+                        // if on the same letter, qm and qn move to distinguishable states
+                        if (D[qm][qn] || D[qm][qn]) {
+                            dist(i, j);
+                            distinguished = true;
+                        }
+
+                        if (distinguished) {
+                            break;
+                        }
+                    }
+
+                    if (!distinguished) {
+                        // qm and qn are indistinguishable
+                        for (int k = 0; k < qi.transitions.length; k++) {
+                            int qm = qi.transitions[k];
+                            int qn = qj.transitions[k];
+
+                            if (qm < qn && !(i == qm && j == qn)) {
+                                S.get(qm).get(qn).add(new Point(qm, qn));
+                            } else if (qm > qn && !(i == qn && j == qm)) {
+                                S.get(qn).get(qm).add(new Point(qn, qm));
+                            }
+                        }
+                    }
+
+                }
+            }
+
+
+        }
+
+        private void dist(int i, int j) {
+            D[i][j] = true;
+
+            HashSet<Point> sSet = S.get(i).get(j);
+            for (Point pair : sSet) {
+                sSet.remove(pair);
+                dist(pair.x, pair.y);
+            }
 
         }
 
